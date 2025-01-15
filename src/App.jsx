2d185ad8d9993ce61962d6df6900aeec25567e01
Aziz TAS <tasaziz4106@gmail.com>
2025-01-15 22:36:46 +0100

    first commit in frontend directory

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { Link, Outlet } from "react-router-dom";
+import logo from "./assets/hoaxify.png";
+import { LanguageSelector } from "./shared/components/LanguageSelector";
+import { useTranslation } from "react-i18next";
+
+function App() {
+  const {t}=useTranslation();
+  return (
+    <>
+      <nav className="navbar navbar-expand bg-body-tertiary shadow-sm">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/">
+            <img src={logo} width={60} />
+            Hoaxify
+          </Link>
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link" to="/signup">
+                {t('signUp')}
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <div className="container mt-2">
+        <Outlet />
+        <LanguageSelector />
+      </div>
+    </>
+  );
+}
+
+export default App;
