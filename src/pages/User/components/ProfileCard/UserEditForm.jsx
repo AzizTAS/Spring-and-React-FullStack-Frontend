b4c46cb143e23bf00cfe2969576cfd24de6667a5
Aziz TAS <tasaziz4106@gmail.com>
2025-03-28 17:45:28 +0100

    Often used components refactored, custom hooks added and new components for clients added.

--- src/pages/User/components/ProfileCard/UserEditForm.jsx
+++ src/pages/User/components/ProfileCard/UserEditForm.jsx
@@ -6,7 +6,7 @@ import { Input } from "@/shared/components/Input";
 import { Alert } from "@/shared/components/Alert";
 import { Button } from "@/shared/components/Button";
 
-export function UserEditForm({ setEditMode }) {
+export function UserEditForm({ setEditMode, setTempImage }) {
   const authState = useAuthState();
   const { t } = useTranslation();
   const [newUsername, setNewUsername] = useState(authState.username);
@@ -14,16 +14,42 @@ export function UserEditForm({ setEditMode }) {
   const [errors, setErrors] = useState({});
   const [generalError, setGeneralError] = useState();
   const dispatch = useAuthDispatch();
+  const [newImage, setNewImage] = useState();
 
   const onChangeUsername = (event) => {
     setNewUsername(event.target.value);
-    setErrors({});
+    setErrors(function (lastErrors) {
+      return {
+        ...lastErrors,
+        username: undefined,
+      };
+    });
   };
 
   const onClickCancel = () => {
     setEditMode(false);
     setNewUsername(authState.username);
+    setNewImage();
+    setTempImage();
   };
+
+  const onSelectImage = (event) => {
+    setErrors(function (lastErrors) {
+      return {
+        ...lastErrors,
+        image: undefined,
+      };
+    });
+    if(event.target.files.length < 1) return;
+    const file = event.target.files[0]
+    const fileReader = new FileReader();
+    fileReader.onloadend = () => {
+      const data = fileReader.result
+      setNewImage(data);
+      setTempImage(data);
+    }
+    fileReader.readAsDataURL(file);
+  }
 
   const onSubmit = async (event) => {
     event.preventDefault();
@@ -31,10 +57,10 @@ export function UserEditForm({ setEditMode }) {
     setErrors({});
     setGeneralError();
     try {
-      await updateUser(authState.id, { username: newUsername });
+      const { data } = await updateUser(authState.id, { username: newUsername, image: newImage });
       dispatch({
         type: "user-update-success",
-        data: { username: newUsername },
+        data: { username: data.username, image: data.image },
       });
       setEditMode(false);
     } catch (axiosError) {
@@ -58,6 +84,12 @@ export function UserEditForm({ setEditMode }) {
         defaultValue={authState.username}
         onChange={onChangeUsername}
         error={errors.username}
+      />
+      <Input
+        label="Profile Image"
+        type="file"
+        onChange={onSelectImage}
+        error={errors.image}
       />
       {generalError && <Alert styleType="danger">{generalError}</Alert>}
       <Button apiProgress={apiProgress} type="submit">
--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,8 @@ import App from "@/App";
 import { Activation } from "@/pages/Activation";
 import { User } from "@/pages/User";
 import { Login } from "@/pages/Login";
+import { PasswordResetRequest } from "@/pages/PasswordReset/Request";
+import { SetPassword } from "@/pages/PasswordReset/SetPassword";
 
 export default createBrowserRouter([
     {
@@ -32,7 +34,15 @@ export default createBrowserRouter([
         {
           path: '/login',
           Component: Login
-        }
+        },
+        {
+          path: "/password-reset/request",
+          Component: PasswordResetRequest
+        },
+        {
+          path: "/password-reset/set",
+          Component: SetPassword
+        },
       ]
     }
   ])
